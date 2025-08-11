91dbac66b2dca8cd391204ff60495968dcee5841
tailwindTactician <>
2025-08-11 18:00:07 +0500

    commit

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿
+//Task 7
+//void Sum(int a)
+//{
+//    int cnt = 0;
+//    for (int i = a; i <= 0; i /= 10)
+//    {
+//        int b = i % 10;
+//         cnt += b;
+//    }
+//    System.Console.WriteLine($"The sum of the digits of the number {a} is : {cnt} ");
+//}
+//int a = Convert.ToInt32(Console.ReadLine());
+//Sum(a);
+
+//Task 8
+// void swap (ref int a, ref int b)
+// {
+//     int cnt = 0;
+//     cnt = 0;
+//     a = b;
+//     b = cnt;
+//     System.Console.WriteLine($"X = {a}");
+//     System.Console.WriteLine($"Y = {b}");
+// }
+// System.Console.Write("X = ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Y = ");
+// int b = Convert.ToInt32(Console.ReadLine());
+//    swap(ref a, ref b);
+
+
+//Task 10
+//void PrintEven(int a, int b) 
+//{
+//    if (a > b)
+//    {
+//        return;
+//    }
+//
+//    if (a % 2 == 0)
+//    {
+//        Console.Write(a + " ");
+//    }
+//
+//    PrintEven(a + 1, b);
+//}
+//
+//void PrintOdd(int a, int b) 
+//{
+//    if (a > b)
+//    {
+//        return;
+//    }
+//
+//    if (a % 2 != 0)
+//    {
+//        Console.Write(a + " ");
+//    }
+//
+//    PrintOdd(a + 1, b);
+//}
+//
+//PrintEven(1, 20);
+//System.Console.WriteLine();
+//PrintOdd(1, 20);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
